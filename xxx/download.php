16c7d4393adaa305db7f0800a5a9d083d61dc0ed
diggydobyte <139668214+enlau@users.noreply.github.com>
2026-06-24 13:50:08 +0000

    php8 compat

--- xxx/download.php
+++ xxx/download.php
@@ -5,99 +5,129 @@ header("HTTP/1.1 200 OK");
 require_once 'functions.php';
 require_once 'td.php';
 
+/*
+|--------------------------------------------------------------------------
+| Safe URL handling
+|--------------------------------------------------------------------------
+| Old code used:
+|   $url = explode("/", $REQUEST_URI);
+|   $url[2], $url[3], $heina[3]
+|
+| That breaks when path depth changes (subdomain, folder move, etc.)
+*/
+$default_kw = $default_kw ?? 'home';
+$segment = app_last_segment();
+$segment = is_string($segment) ? $segment : '';
 
-$url = explode("/",$REQUEST_URI);
-$torrent = new Torrent( './data/data.torrent');
-$parts = preg_split("/(?<![\\\])\./", $_SERVER["REQUEST_URI"], -1, 
-PREG_SPLIT_NO_EMPTY);
-$heina = explode("/", $parts[0]);
-$mnhd = str_replace("%20", " ", $heina[3]);
+$pathInfo = pathinfo(app_request_path());
+$mnhd = urldecode($pathInfo['filename'] ?? '');
 
-// getting rid or extension and dots in torrent comment
-$torrentnameWithoutExtension = explode(".torrent", $url[3]);
-$torrentnameWithoutDots = str_replace("."," ", $torrentnameWithoutExtension); // normal letters
-$torrentname = strtoupper($torrentnameWithoutDots[0]); // torrent comment in caps
+/*
+|--------------------------------------------------------------------------
+| Torrent object setup
+|--------------------------------------------------------------------------
+*/
+$torrent = new Torrent('./data/data.torrent');
 
+/*
+|--------------------------------------------------------------------------
+| Build torrent comment safely from requested .torrent name
+|--------------------------------------------------------------------------
+| Old code tried to use:
+|   $url[3]
+*/
+$torrentBaseName = preg_replace('/\.torrent$/i', '', urldecode($segment));
+$torrentnameWithoutDots = str_replace('.', ' ', $torrentBaseName);
+$torrentname = strtoupper($torrentnameWithoutDots);
 
-$torrent->comment("$torrentnameWithoutDots[0]");
+$torrent->comment($torrentnameWithoutDots);
 $torrent->save('torrents/1.torrent'); // save to disk
 
+/*
+|--------------------------------------------------------------------------
+| Handle .torrent request
+|--------------------------------------------------------------------------
+*/
+if ($segment !== '' && preg_match('/\.torrent$/i', $segment)) {
+    global $_c, $torrentlocation, $maxnotorrent, $kw;
 
-if(preg_match("/.torrent/",$url[3])) {
-	global $url,$_c,$torrentlocation,$maxnotorrent,$kw;
-    $fp=fopen("$torrentlocation/1.torrent",'r');
-    $dest = fopen("$torrentlocation/$url[3]", 'w');  
-    echo stream_copy_to_stream($fp, $dest);
-    $myspecialfilename = "$torrentlocation/$url[3]";
-    fclose ($fp);
-    fclose($dest); 
-	header('Location: '.$url[3]);
-	exit;
-	die;
+    $requestedTorrent = basename($segment);
+    $sourceTorrent = $torrentlocation . '/1.torrent';
+    $targetTorrent = $torrentlocation . '/' . $requestedTorrent;
 
-} 
+    $fp = fopen($sourceTorrent, 'r');
+    $dest = fopen($targetTorrent, 'w');
 
+    if ($fp && $dest) {
+        stream_copy_to_stream($fp, $dest);
+        fclose($fp);
+        fclose($dest);
 
-if(preg_match("/-page-/",$url[2])) {
+        header('Location: ' . $requestedTorrent);
+        exit;
+    }
 
-	$mkw = preg_match("#(.+)-page-#si",$url[2],$matches);
+    if ($fp) {
+        fclose($fp);
+    }
+    if ($dest) {
+        fclose($dest);
+    }
 
-	$mkw = str_replace('-',' ',$matches[1]);
-
-} else {
-
-	$mkw = preg_match("#(.+).html#si",$url[2],$matches);
-
-	$mkw = str_replace('-',' ',$matches[1]);
-
+    die('Could not create torrent file.');
 }
 
-if(preg_match("/.jpg/",$url[3]))  {
+/*
+|--------------------------------------------------------------------------
+| Extract keyword + page safely
+|--------------------------------------------------------------------------
+| Old code used $url[2]
+*/
+list($mkw, $pag) = app_extract_keyword_and_page($segment, $default_kw);
 
-	global $url,$_c,$authorImageLocation,$maxpic;
-	srand((double)microtime()*1000000);
-	$rnd = rand() % $maxpic + 1;
-	$contents = '';
-    $fp=fopen("$authorImageLocation/$rnd.jpg",'r');
-    if($fp) {while(!feof($fp)) {$contents.=fread($fp,1024);}}
-    fclose ($fp); 
-	header('Content-type: image/jpeg');
-	echo $contents;
+/*
+|--------------------------------------------------------------------------
+| Handle .jpg request
+|--------------------------------------------------------------------------
+*/
+if ($segment !== '' && preg_match('/\.jpg$/i', $segment)) {
+    global $_c, $authorImageLocation, $maxpic;
 
+    srand((double)microtime() * 1000000);
+    $rnd = rand() % $maxpic + 1;
 
-	die;
-} 
+    $contents = '';
+    $fp = fopen("$authorImageLocation/$rnd.jpg", 'r');
 
+    if ($fp) {
+        while (!feof($fp)) {
+            $contents .= fread($fp, 1024);
+        }
+        fclose($fp);
+    }
 
-$pag = preg_match("#-page-(.+).html#si",$url[2],$matches);
-
-$pag = $matches[1];
-
-if($pag =='') $pag=1;
-
-
-
-if($mkw == '') {
-
-	$mkw = $default_kw;
-
-	$pag = 1;
-
+    header('Content-type: image/jpeg');
+    echo $contents;
+    die;
 }
 
-
-if(preg_match('/torrent-archive/',$url[2])) {
-
-	show_archive();
-
-	$tpl->display('archive.tpl');
-
-	die;	
-
+/*
+|--------------------------------------------------------------------------
+| Archive handling
+|--------------------------------------------------------------------------
+*/
+if ($segment !== '' && preg_match('/torrent-archive/i', $segment)) {
+    show_archive();
+    $tpl->display('archive.tpl');
+    die;
 }
 
+/*
+|--------------------------------------------------------------------------
+| Normal page rendering
+|--------------------------------------------------------------------------
+*/
 set_default();
-
+$tpl->assign('currentPage', $pag);
 $tpl->display('download.tpl');
-
 ?>
